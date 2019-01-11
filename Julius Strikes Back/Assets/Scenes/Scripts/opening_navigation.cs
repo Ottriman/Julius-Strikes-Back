07bc848cc25e321f9987a776560c55b3529d0ccd
Playergh <kari.garpsson@gmail.com>
2019-01-11 16:54:52 +0000

    byrja að vinna á intro cutscene
    METAL EAR

---
+++ Julius Strikes Back/Assets/Scenes/Scripts/opening_navigation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class opening_navigation : MonoBehaviour {
+
+    private int slide_id;
+
+    // Use this for initialization
+	void Start () {
+        slide_id = 0;
+	}
+
+    public void next_scene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("x"))
+        {
+            next_scene();
+        }
+        /*else if (Input.GetKey("c"))
+        {
+            
+            //Things
+        }*/
+    }
+}
