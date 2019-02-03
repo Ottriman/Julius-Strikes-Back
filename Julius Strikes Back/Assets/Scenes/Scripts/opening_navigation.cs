8f08aafa8dbed4c9a34b4c0a0090d63b1fd3856f
Playergh <kari.garpsson@gmail.com>
2019-02-03 16:49:17 +0000

    bætt við placeholder standing animation
    it just works

--- Julius Strikes Back/Assets/Scenes/Scripts/opening_navigation.cs
+++ Julius Strikes Back/Assets/Scenes/Scripts/opening_navigation.cs
@@ -6,18 +6,30 @@ using UnityEngine.UI;
 
 public class opening_navigation : MonoBehaviour {
 
-    static int size = 5;
-    public GameObject[] slides;
-    public int slide_id;
+    static int size = 7;
+    private GameObject[] slides;
+    private int slide_id;
+
+    public GameObject slide1;
+    public GameObject slide2;
+    public GameObject slide3;
+    public GameObject slide4;
+    public GameObject slide5;
+    public GameObject slide6;
+    public GameObject slide7;
 
     // Use this for initialization
     void Start () {
         slide_id = 0;
         slides = new GameObject[size];
-        for (int i = 0; i < size; i++)
-        {
-            slides[i] = GameObject.FindWithTag("cutscene_slide");
-        }
+
+        slides[0] = slide1;
+        slides[1] = slide2;
+        slides[2] = slide3;
+        slides[3] = slide4;
+        slides[4] = slide5;
+        slides[5] = slide6;
+        slides[6] = slide7;
     }
 
     public void next_scene()
@@ -27,17 +39,14 @@ public class opening_navigation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey("x"))
-        {
-            next_scene();
-        }
-        if(Input.GetMouseButtonDown(0) && slide_id < size)
+        
+        if(Input.anyKeyDown && slide_id < size-1)
         {
             slides[slide_id].SetActive(false);
             slides[slide_id+1].SetActive(true);
             slide_id++;
         }
-        else if(Input.GetMouseButtonDown(0) && slide_id >= size)
+        else if(Input.anyKeyDown && slide_id >= size-1)
         {
             next_scene();
         }
