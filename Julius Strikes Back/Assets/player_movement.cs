185badc2ac151f2f70a16f8703bafcba3fa0ec63
Playergh <kari.garpsson@gmail.com>
2019-02-06 15:55:21 +0000

    byrjaðir á hreyfingu

---
+++ Julius Strikes Back/Assets/player_movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    float horizontal_move = 0f;
+    float vertical_move = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontal_move = Input.GetAxisRaw("Horizontal");
+        vertical_move = Input.GetAxisRaw("Vertical");
+	}
+
+    private void FixedUpdate()
+    {
+        //Move character
+
+        controller.Move(horizontal_move, false, false);
+        controller.Move(vertical_move, false, false);
+    }
+}
