c5849f7d4f12081c4b3721076f11080fd5839e4f
Playergh <kari.garpsson@gmail.com>
2019-02-18 15:52:05 +0000

    save episode 2 attack of the clönes
    load virkar en ekki save

--- Julius Strikes Back/Assets/Scenes/Scripts/player.cs
+++ Julius Strikes Back/Assets/Scenes/Scripts/player.cs
@@ -8,13 +8,26 @@ public class player : MonoBehaviour {
     public int crosses = 0;
     public string file_name;
     
-    // Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SavePlayer()
+    {
+        save_and_load_progress.save(this);
+    }
+
+    public void LoadPlayer()
+    {
+        player_data data = save_and_load_progress.load();
+
+        if(data.file_name == null)
+        {
+            Debug.LogError("file doesn't exist");
+        }
+        else
+        {
+            level = data.level;
+            crosses = data.crosses_smashed;
+            file_name = data.file_name;
+        }
+
+        Debug.Log("Tried to load " + file_name);
+    }
 }
--- Julius Strikes Back/Assets/Scenes/Scripts/save_and_load_progress.cs
+++ Julius Strikes Back/Assets/Scenes/Scripts/save_and_load_progress.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
-[System.Serializable]
 public static class save_and_load_progress {
 
     public static void save(player player_chosen)
